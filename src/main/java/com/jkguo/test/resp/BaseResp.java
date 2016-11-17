6f77da67e406982574b8c06c332c724d7898104e
郭锦坤 <guojinkun@qtone.cn>
2016-11-17 16:30:30 +0800

    修改基础请求返回类

--- src/main/java/com/jkguo/test/resp/BaseResp.java
+++ src/main/java/com/jkguo/test/resp/BaseResp.java
@@ -21,16 +21,8 @@ public class BaseResp {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     public Object getData() {
