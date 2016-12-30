d82734c58cf3541782ef975cc159da5b0fad9217
jkguo <oofgdoo@163.com>
2016-12-31 02:03:27 +0800

    测试

--- src/main/java/com/jkguo/test/controller/AreaController.java
+++ src/main/java/com/jkguo/test/controller/AreaController.java
@@ -4,14 +4,12 @@ import com.jkguo.test.enums.RespResultEnum;
 import com.jkguo.test.resp.BaseResp;
 import com.jkguo.test.resp.bean.AreaBean;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Array;
 import java.util.*;
 
 /**
